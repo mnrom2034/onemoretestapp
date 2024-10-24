3ba708e3a6c9c1c48abb21d22eb1e27d4247fa32
mnrom2034 <120595976+mnrom2034@users.noreply.github.com>
2024-10-24 23:39:32 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -45,5 +45,5 @@ def upload_file():
     return jsonify({"error": "Failed to download file"}), 500
 
 # This is the entry point for Vercel
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+#if __name__ == "__main__":
+    #app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
